2207d194208d3e458a9882e837e58b2f1abb092c
mchen <mchen@aaxiscommerce.com>
2017-11-19 21:31:01 +0800

    Upgrade to scalaVersion 2.12.4 and other dependencies to latest Change to use scalatest

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ organization := "ru.circumflex"
 
 version := "2.1.1"
 
-scalaVersion := "2.9.1"
+scalaVersion := "2.12.4"
 
 scalacOptions ++= Seq("-deprecation", "-unchecked")
 
@@ -16,9 +16,10 @@ publishMavenStyle := true
 
 libraryDependencies ++= Seq(
   "org.freemarker"   		% 	"freemarker"  			% "2.3.18",
-  "net.liftweb"         	%% 	"lift-util"          	% "2.4-SNAPSHOT" 	% "compile",
-  "net.liftweb"         	%% 	"lift-common"          	% "2.4-SNAPSHOT" 	% "compile",
-  "commons-beanutils"   	% 	"commons-beanutils"     % "1.8.0",
-  "commons-io"   			% 	"commons-io"      		% "2.1",
-  "org.apache.commons"   	% 	"commons-lang3"      	% "3.1" 			% "test",
-  "org.scala-tools.testing" %% 	"specs" 				% "1.6.9" 			% "test")
+  "net.liftweb"         	%% 	"lift-util"          	% "3.1.1" 	% "compile",
+  "net.liftweb"         	%% 	"lift-common"          	% "3.1.1" 	% "compile",
+  "commons-beanutils"   	% 	"commons-beanutils"     % "1.9.3",
+  "commons-io"   			% 	"commons-io"      		% "2.6",
+  "org.apache.commons"   	% 	"commons-lang3"      	% "3.7" 			% "test",
+  "org.scalatest" %% "scalatest" % "3.0.4" % "test"
+)
--- src/test/scala/specs.scala
+++ src/test/scala/specs.scala
@@ -1,29 +1,32 @@
 package ru.circumflex
 package freemarker
 
-import org.specs.Specification
 import org.apache.commons.io.FileUtils
 import org.apache.commons.lang3.StringUtils
-import org.specs.matcher.Matcher
 import java.io.File
+
+import org.scalatest.matchers.{MatchResult, Matcher}
+import org.scalatest.{FlatSpec, Matchers}
+
 import collection.mutable.HashMap
 
-class CircumflexFtlSpec extends Specification {
+class CircumflexFtlSpec extends FlatSpec with Matchers {
 
   def beFine(root: Any) = new Matcher[String] {
-    def apply(name: => String) = {
+
+    override def apply(name : String): MatchResult = {
       val text = ftl2string("/" + name + ".ftl", root).trim
       val out = FileUtils.readFileToString(
         new File(this.getClass.getResource("/" + name + ".out").toURI), "UTF-8").trim
       val diffIndex = StringUtils.indexOfDifference(text, out)
       val diff = StringUtils.difference(text, out)
-      (diffIndex == -1,
+      MatchResult(diffIndex == -1,
           "\"" + name + "\" is fine",
           "\"" + name + "\" fails at " + diffIndex + ": " + StringUtils.abbreviate(diff, 32))
     }
   }
 
-  "Circumflex FTL" should {
+  "Circumflex FTL" should
     "handle simple objects" in {
       object simpleObject {
         val name = "Joe"
@@ -34,23 +37,23 @@ class CircumflexFtlSpec extends Specification {
       }
       
       val obj = simpleObject
-      "Simple objects" must beFine(Map("obj"->obj))
+      "Simple objects" should beFine (Map("obj"->obj))
     }
-    "handle lists" in {
+    it should "handle lists" in {
       val list = List("one", "two", "three")
       val range = 0 to 9
-      "Lists" must beFine(Map("list"->list, "range" -> range))
+      "Lists" should beFine(Map("list"->list, "range" -> range))
     }
-    "handle maps" in {
+    it should "handle maps" in {
       val map = new HashMap[String, Any]
       map += ("one" -> "Hello to")
       map += ("two" -> HashMap("one" -> "Joe", "two" -> "Smith"))
       //val map = Map("one" -> "Hello to", "two" -> Map("one" -> "Joe", "two" -> "Smith"))
       val context = Map("map" -> map)
       println(context)
-      "Maps" must beFine(context)
+      "Maps" should beFine(context)
     }
-    "provide limited support of Scala XML" in {
+    it should "provide limited support of Scala XML" in {
       val root = <root>
         <child>
           <one id="0">1</one>
@@ -59,8 +62,8 @@ class CircumflexFtlSpec extends Specification {
         </child>
           <child/>
       </root>
-      "XML" must beFine(Map("root"->root))
+      "XML" should beFine(Map("root"->root))
     }
-  }
+
 
 }
